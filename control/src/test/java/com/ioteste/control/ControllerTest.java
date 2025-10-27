8764a01d243178b9d511063c6a0dc82cc4c39cdb
bot0014 <markgitjub@outlook.com>
2025-10-27 04:10:30 -0300

    test evitar apagón

--- control/src/test/java/com/ioteste/control/ControllerTest.java
+++ control/src/test/java/com/ioteste/control/ControllerTest.java
@@ -2,7 +2,6 @@ package com.ioteste.control;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -124,7 +123,7 @@ public class ControllerTest {
     
     
     @Test
-    public void testSwitchTurnsOff()throws JsonProcessingException{
+    public void testSwitchTurnsOff() throws JsonProcessingException {
         String siteConfig = """
                             {
                                 "site": "oficina001",
@@ -232,7 +231,115 @@ public class ControllerTest {
         assertFalse(result.get(0).isPower());
     }
     
-    
+    @Test
+    public void testAboveMaxEnergy() throws JsonProcessingException {
+        String siteConfig = """
+                            {
+                                "site": "oficina001",
+                                "maxEnergy": "14 kWh",
+                                "timeSlot": {
+                                  "contractType":"std",
+                                  "refreshPeriod":"10000 ms"
+                                  },
+                                "rooms": [
+                                    {
+                                        "name": "office1",
+                                        "expectedTemp": "22",
+                                        "energy": "8 kWh",
+                                        "switch": "http://host:port/switch/1",
+                                        "sensor": "mqtt:topic1"
+                                    },
+                                    {
+                                        "name": "shellyhtg3-84fce63ad204",
+                                        "expectedTemp": "21",
+                                        "energy": "8 kWh",
+                                        "switch": "http://host:port/switch/2",
+                                        "sensor": "mqtt:topic2"
+                                    }
+                                ]
+                            }""";
+        String sensorData = """
+                            {
+                              "src": "shellyhtg3-84fce63ad204",
+                              "dst": "ht-suite/events",
+                              "method": "NotifyFullStatus",
+                              "params": {
+                                "ts": 1752192302.55,
+                                "ble": {},
+                                "cloud": {
+                                  "connected": false
+                                },
+                                "devicepower:0": {
+                                  "id": 0,
+                                  "battery": {
+                                    "V": 5.32,
+                                    "percent": 65
+                                  },
+                                  "external": {
+                                    "present": false
+                                  }
+                                },
+                                "ht_ui": {},
+                                "humidity:0": {
+                                  "id": 0,
+                                  "rh": 58.9
+                                },
+                                "mqtt": {
+                                  "connected": true
+                                },
+                                "sys": {
+                                  "mac": "84FCE63AD204",
+                                  "restart_required": false,
+                                  "time": null,
+                                  "unixtime": null,
+                                  "uptime": 1,
+                                  "ram_size": 256644,
+                                  "ram_free": 120584,
+                                  "fs_size": 1048576,
+                                  "fs_free": 774144,
+                                  "cfg_rev": 14,
+                                  "kvs_rev": 0,
+                                  "webhook_rev": 0,
+                                  "available_updates": {},
+                                  "wakeup_reason": {
+                                    "boot": "deepsleep_wake",
+                                    "cause": "periodic"
+                                  },
+                                  "wakeup_period": 7200,
+                                  "reset_reason": 8
+                                },
+                                "temperature:0": {
+                                  "id": 0,
+                                  "tC": 20,
+                                  "tF": 67.8
+                                },
+                                "wifi": {
+                                  "sta_ip": "192.168.1.81",
+                                  "status": "got ip",
+                                  "ssid": "IOTNET",
+                                  "rssi": -68
+                                },
+                                "ws": {
+                                  "connected": false
+                                }
+                              }
+                            }""";
+        DataSite dSite;
+        DataSensor dSensor;
+        List<DataSwitch> dSwitch = new ArrayList<>();
+        
+        dSite = new DataSite(siteConfig);
+        dSensor = new DataSensor(sensorData);
+        dSwitch.add(new DataSwitch("http://host:port/switch/1", true));
+        dSwitch.add(new DataSwitch("http://host:port/switch/2", false));
+        
+        Controller instance = new ControllerImpl();
+        List<Operation> result = instance.powerManagement(dSite, dSensor, dSwitch);
+        
+        assertEquals(1, result.size());
+        assertEquals("http://host:port/switch/2", result.get(0).getSwitchURL());
+        assertFalse(result.get(0).isPower());
+    }
     
     public class ControllerImpl implements Controller {
 
@@ -241,13 +348,45 @@ public class ControllerTest {
             
             List<Operation> operations = new ArrayList<>();
             
+            float currentEnergy = getCurrentEnergy(siteConfig, switchStatus);
+            
             for (Room r : siteConfig.getRooms()) {
+                boolean isActiveSwitch = isActiveRoomSwitch(r, switchStatus);
+                
                 if (sensorData.getSrc().equals(r.getName())) {
-                    boolean turnOn = sensorData.getTemperature() < r.getExpectedTemp();
-                    operations.add(new Operation(r.getSwitchURL(), turnOn));
+                    if (!isActiveSwitch && currentEnergy + r.getEnergy() > siteConfig.getMaxEnergy()) {
+                        boolean power = false;
+                        operations.add(new Operation(r.getSwitchURL(), power));
+                    } else {
+                        boolean power = sensorData.getTemperature() < r.getExpectedTemp();
+                        operations.add(new Operation(r.getSwitchURL(), power));
+                    }
                 }  
             }
             return operations;
+        }
+        
+        private float getCurrentEnergy(DataSite siteConfig, List<DataSwitch> switchStatus) {
+            float currentEnergy = 0;
+            
+            for (Room r : siteConfig.getRooms()) {
+                for (DataSwitch dSwitch : switchStatus) {
+                    if (dSwitch.isActive() && dSwitch.getSwitchURL().equals(r.getSwitchURL())) {
+                        currentEnergy += r.getEnergy();
+                    }
+                }
+            }
+            
+            return currentEnergy;
+        }
+        
+        private boolean isActiveRoomSwitch(Room r, List<DataSwitch> switchStatus) {
+            for (DataSwitch dSwitch : switchStatus) {
+                if (r.getSwitchURL().equals(dSwitch.getSwitchURL())) {
+                    return dSwitch.isActive();
+                }
+            }
+            return true;
         }
         
     }
