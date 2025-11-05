9cdfe19c8be8079c431200be887a8f1078b3f720
bot0014 <markgitjub@outlook.com>
2025-11-05 06:57:11 -0300

    avances httpclient

--- app/src/main/java/com/ioteste/app/App.java
+++ app/src/main/java/com/ioteste/app/App.java
@@ -1,8 +1,38 @@
 package com.ioteste.app;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
+    private HttpClient client = HttpClient.newHttpClient();
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+    }
+    
+    private String getSwitchStatus(String switchURL) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(switchURL)).build();
+        HttpResponse<String> response = this.client.send(request, BodyHandlers.ofString());
+        
+        return response.body();
+    }
+    
+    private String postSwitchOp(String switchURL, String jsonBody) throws IOException, InterruptedException {
+        BodyPublisher bodyPublisher = BodyPublishers.ofString(jsonBody);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(switchURL))
+                .header("Content-Type", "application/json")
+                .POST(bodyPublisher)
+                .build();
+        HttpResponse<String> response = this.client.send(request, BodyHandlers.ofString());
+        
+        return response.body();
     }
 }
