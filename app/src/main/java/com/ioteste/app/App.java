2dcde4b92e304d461451c6480c8abb947b994c0b
Darlinx25 <fsalaberry505@gmail.com>
2025-11-07 11:46:27 -0300

    Cliente MQTT

--- app/src/main/java/com/ioteste/app/App.java
+++ app/src/main/java/com/ioteste/app/App.java
@@ -9,11 +9,17 @@ import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import java.util.UUID;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 public class App {
     private HttpClient client = HttpClient.newHttpClient();
+    private MqttClient mqttClient;
 
     public static void main(String[] args) {
-        
+        App myApp = new App();
+        myApp.start();
     }
     
     private String getSwitchStatus(String switchURL) throws IOException, InterruptedException {
@@ -35,4 +41,52 @@ public class App {
         
         return response.body();
     }
+    
+    private void start(){
+        String brokerUrl = "tcp://mosquitto:1883";
+        String topic = "habitacion/ambiente"; 
+        String clientId = "app-" + UUID.randomUUID().toString();
+
+        try {
+            mqttClient = new MqttClient(brokerUrl, clientId, new MemoryPersistence());
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setAutomaticReconnect(true);
+            options.setCleanSession(true);
+            options.setConnectionTimeout(10);
+
+            mqttClient.setCallback(new MqttCallback() {
+                
+                @Override
+                public void connectionLost(Throwable cause) {
+                    System.err.println("Conexión perdida: " + cause.getMessage());
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) {
+                    String payload = new String(message.getPayload());
+                    
+                    
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken token) {
+                    // (Nosotros no utilizamos este, pero es necesario declararlo)
+                }
+            });
+            mqttClient.connect(options);
+            mqttClient.subscribe(topic);
+            while (true) {
+                Thread.sleep(1000);
+            }
+        } catch (MqttException | InterruptedException e) {
+            System.err.println("Error en el cliente MQTT: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
